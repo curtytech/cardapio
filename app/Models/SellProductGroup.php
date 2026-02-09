a64493180d4a870213d92cc0509c6bff353a1e79
Phelipe Curty <71573615+curtytech@users.noreply.github.com>
2026-02-09 15:18:57 -0300

    updates sell

--- app/Models/SellProductGroup.php
+++ app/Models/SellProductGroup.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class ProductQuantity extends Model
+class SellProductGroup extends Model
 {
-    protected $table = 'products_quantities';
+    protected $table = 'sell_products_groups';
 
     public $timestamps = false;
 
